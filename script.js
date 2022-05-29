6f558dbadfee06fe0b2c1fbcceb3bc822bfb7d9e
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-05-28 22:00:48 -0400

    adding compare function

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 $(document).ready(function() {
 
-//Weather API
+// Weather API //
 const apiKey = "52926e6cab59a5ac00ea29afe5895e9f";
 
-const cityEl
-const dataEl
-const weatherIconEl
-const temperatureEl
-const humidityEl
-const windEl
-const uvIndexEl
-const cityListEl
+// Display Weather //
+const cityEl = $("h2#city");
+const dataEl = $("h3#date");
+const weatherIconEl = $("img#weather-icon");
+const temperatureEl = $("span#temperature");
+const humidityEl = $("span#whumidity")
+const windEl = $("span#wind");
+const uvIndexEl = $("span#uv-index");
+const cityListEl = $("div.cityList");
+
+// Input City //
+const cityInput =$("#city-input");
+
+// Previously Searched Cities //
+let pastCities = [];
+
+function compare(a,b) {
+    const cityA = a.city.toUpperCase();
+    const cityB = b.city.toUpperCase();
+
+    let comparison = 0;
+    if (cityA > cityB) {
+        comparison =1;
+        } else if (cityA < cityB) {
+            comparison =-1;
+        }
+        return comparison;
+    }
+
+    
 
 
 
