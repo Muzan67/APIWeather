96b696c9db55363a4b65614cb8e0cbb787ced3ca
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-05-28 21:09:25 -0400

    adding const js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-api.openweathermap.org/data/2.5/onecall?lat=38.8&lon=12.09&callback=test
+$(document).ready(function() {
 
-http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={52926e6cab59a5ac00ea29afe5895e9f}
+//Weather API
+const apiKey = "52926e6cab59a5ac00ea29afe5895e9f";
+
+const cityEl
+const dataEl
+const weatherIconEl
+const temperatureEl
+const humidityEl
+const windEl
+const uvIndexEl
+const cityListEl
+
+
+
+
+
+
+
+
+
+})
+
